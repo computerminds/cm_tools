c977c3136d3e3f9755e668fc13d8a5628db98344
James Silver <jamsilver@gmail.com>
2012-03-29 10:58:39 +0100

    'Bare' views style with no row-wrappers

---
+++ theme/views-view-cm-tools-bare.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file views-view-cm-tools-bare.tpl.php
+ * Default simple view template to display a list of rows but without the
+ * wrapping markup.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <?php print $row; ?>
+<?php endforeach; ?>
